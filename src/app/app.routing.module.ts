af2e66fcca430c8e64bc2277fa69c366ed86ef92
Antonio <antonioadamipalma@gmail.com>
2023-01-21 09:25:45 -0300

    Admin, login and user pages

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductIdGuard } from './guards/product-id.guard';
+import { AdminComponent } from './pages/admin/admin.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ConfirmationComponent } from './pages/confirmation/confirmation.component';
 import { DetailsComponent } from './pages/details/details.component';
 import { ErrorComponent } from './pages/error/error.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,6 +21,9 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'checkout', component: CheckoutComponent },
   { path: 'confirmation', component: ConfirmationComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: 'error' }
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '../components/components.module';
+import { AdminComponent } from './admin/admin.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { DetailsComponent } from './details/details.component';
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { HomeComponent } from './home/home.component';
     DetailsComponent,
     ErrorComponent,
     CheckoutComponent,
-    ConfirmationComponent
+    ConfirmationComponent,
+    AdminComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
